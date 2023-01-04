50d235b8273004c5c0459e02e72d0c53969f18b4
Tucker Barton <hisashiburix@gmail.com>
2023-01-04 17:41:27 -0500

    Add API integration to client-app/App.tsx. Modify API/Program.cs to allow for CORs from localhost client-app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  // Get activities data from API on first load
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((res) => {
+      console.log(res);
+      setActivities(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header className="App-header">
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
